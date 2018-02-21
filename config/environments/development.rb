e6f1b0b72aed1e32809d0ad4f45de399aeafb3df
Nathan L. Walls <nathan.walls@ingramcontent.com>
2018-02-21 10:20:58 -0500

    Don't fight with yarn over the node version

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,4 +25,5 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_mailer.asset_host = "http://localhost:3000"
+  config.webpacker.check_yarn_integrity = false
 end
